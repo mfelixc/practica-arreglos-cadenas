e2a3b099cf66e23bdeb675e9c9d3acc30d48bb58
mfelixc <mfelixc@unjbg.edu.pe>
2026-06-21 11:08:38 -0500

    Use un for para impimir en pantalla el numero de canicas en cada vaso y corregi el arreglo frecuencia al ver los limites del problema tambien coloque el arreglo fuera de main por que al estar adentro provocava un desbordamiento de pila y no se podia compilar

--- canicas.cpp
+++ canicas.cpp
@@ -1,14 +1,17 @@
 #include <iostream>
 using namespace std;
+int frecuencia[1000001] = {0};
 int main() {
     int N, C;
     cin >> N >> C;
-    int frecuencia[1001] = {0}; 
- 
     for (int i = 0; i < C; i++) {
         int vaso;
         cin >> vaso;
         frecuencia[vaso]= frecuencia[vaso] + 1;
+    }
+    
+    for (int i = 1; i <= N; i++) {
+        cout << frecuencia[i] << endl;
     }
 
     return 0;
