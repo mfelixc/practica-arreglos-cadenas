bddcf3abbae0dcc195fc8437f7e77669d46d853d
mfelixc <mfelixc@unjbg.edu.pe>
2026-06-21 10:53:13 -0500

    Recorrí las C canicas lanzadas, leí con la variable vaso para saber donde aumentar en 1 en el arreglo.

--- canicas.cpp
+++ canicas.cpp
@@ -4,6 +4,12 @@ int main() {
     int N, C;
     cin >> N >> C;
     int frecuencia[1001] = {0}; 
-    
+ 
+    for (int i = 0; i < C; i++) {
+        int vaso;
+        cin >> vaso;
+        frecuencia[vaso]= frecuencia[vaso] + 1;
+    }
+
     return 0;
 }
